127833adfbdfafd8042d0b6de1c2048a77588b1f
SFangYy <sfangyy@163.com>
2025-07-01 20:07:29 +0800

    add basic dev docker

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start SSH service
+/usr/sbin/sshd
+
+# Start VNC server for the 'user' user
+# Ensure D-Bus session is launched and environment variables are set.
+# Then, start vncserver, which will pick up the ~/.vnc/xstartup script by default.
+su - user -c "eval \`dbus-launch --sh-syntax\` && \
+exec /usr/bin/vncserver $DISPLAY -geometry $VNC_GEOMETRY -depth $VNC_DEPTH -localhost no"
+
+# Keep the container running
+# This is necessary because vncserver will detach itself.
+tail -f /dev/null
